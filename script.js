91368eae7a19bfd1381f221469b596ea2fc74854
itsvikashkumar93 <itsvikashkumar8270@gmail.co>
2024-07-20 11:15:19 +0530

    Updated

--- script.js
+++ script.js
@@ -89,60 +89,38 @@ const gsapAnimation = () => {
 }
 
 const recentLaunches = () => {
-    var tl = gsap.timeline();
-
-    tl.to('#recentContainer', {
-        transform: "translateX(-300%)",
+    gsap.to('.text-container', {
+        y: '-300%',
+        ease: Power1,
         scrollTrigger: {
-            trigger: "#recent",
-            start: "top 0%",
-            end: 'top -400%',
+            trigger: '#recent',
             pin: true,
-            // markers: true,
-            scrub: 2
+            start: "top top",
+            end: "bottom bottom",
+            endTrigger: ".last-container",
+            scrub: 1,
         }
     })
-    .to("#one-img img", {
-        scale: 1.15,
-        scrollTrigger: {
-            trigger: '#one-img img',
-            start: 'top 10%',
-            end: 'top -15%',
-            // markers: true,
-            scrub: 2
+
+    let elems = document.querySelectorAll('.text-container');
+    Shery.imageEffect('.img-container', {
+        style: 4,
+        config: {onMouse: {value: 1}},
+        // debug: true,
+        slideStyle: (setScroll) => {
+            elems.forEach((elem, index) => {
+                ScrollTrigger.create({
+                    trigger: elem,
+                    start: 'top top',
+                    scrub: 1,
+                    onUpdate: (prog)=>{
+                        console.log(prog);
+                        setScroll(prog.progress+index)
+                    }
+                })
+            })
         }
     })
-    .to("#two-img img", {
-        scale: 1.15,
-        scrollTrigger: {
-            trigger: '#two-img img',
-            start: 'top -30%',
-            end: 'top -90%',
-            // markers: true,
-            scrub: 2
-        }
-    })
-    .to("#three-img img", {
-        scale: 1.15,
-        scrollTrigger: {
-            trigger: '#three-img img',
-            start: 'top -100%',
-            end: 'top -150%',
-            // markers: true,
-            scrub: 2
-        }
-    })
-    .to("#four-img img", {
-        scale: 1.15,
-        scrollTrigger: {
-            trigger: '#four-img img',
-            start: 'top -240%',
-            end: 'top -350%',
-            // markers: true,
-            scrub: 2
-        }
-    })
-        
 }
 
 const sheryAnimation = () => {
@@ -151,6 +129,7 @@ const sheryAnimation = () => {
         gooey: true,
         config: { "uFrequencyX": { "value": 0, "range": [0, 100] }, "uFrequencyY": { "value": 0, "range": [0, 100] }, "uFrequencyZ": { "value": 0, "range": [0, 100] }, "geoVertex": { "range": [1, 64], "value": 1 }, "zindex": { "value": "9", "range": [-9999999, 9999999] }, "aspect": { "value": 2.0463362382580907 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 5.6, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.07, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "noEffectGooey": { "value": false }, "onMouse": { "value": 1 }, "noise_speed": { "value": 1.07, "range": [0, 10] }, "metaball": { "value": 0.14, "range": [0, 2], "_gsap": { "id": 13 } }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 26.72, "range": [0, 100] }, "a": { "value": 0.69, "range": [0, 30] }, "b": { "value": -0.97, "range": [-1, 1] } }
     });
+
 }
 
 recentLaunches();
