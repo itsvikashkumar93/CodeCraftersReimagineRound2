fb3a15a601b1fc7982366d096ff8239c66c4019b
itsvikashkumar93 <itsvikashkumar8270@gmail.co>
2024-07-20 15:06:14 +0530

    Updated

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const recentLaunches = () => {
     let elems = document.querySelectorAll('.text-container');
     Shery.imageEffect('.img-container', {
         style: 4,
-        config: { onMouse: { value: 1 } },
+        config: { "uColor": { "value": false }, "uSpeed": { "value": 0.31, "range": [0.1, 1], "rangep": [1, 10] }, "uAmplitude": { "value": 1.5, "range": [0, 5] }, "uFrequency": { "value": 3.5, "range": [0, 10] }, "geoVertex": { "range": [1, 64], "value": 32 }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7241449917191698 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": false }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.06, "range": [1, 5] }, "scrollType": { "value": 0 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.2, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0.002, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] } },
         slideStyle: (setScroll) => {
             elems.forEach((elem, index) => {
                 ScrollTrigger.create({
@@ -85,7 +85,6 @@ const recentLaunches = () => {
                     scroller: '#main',
                     scrub: 1,
                     onUpdate: (prog) => {
-                        console.log(prog);
                         setScroll(prog.progress + index)
                     }
                 })
